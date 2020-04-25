b073536a80b25ee2644a6a68ee0ca80fa7e6e70d
Diego Silva <41221742+odiegosilva1@users.noreply.github.com>
2020-04-25 14:10:37 -0300

    upate files

---
+++ scripts/title.js
@@ -0,0 +1,12 @@
+
+
+function typeWriter (título) {
+    const textoArray = elemento.innerHTML;
+    console.log(textoArray);
+
+}
+
+
+
+const título = document.querySelector(h1);
+typeWriter(título);
