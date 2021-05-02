b69a0d82ac35441f610e0418ee0422eea6e94b9f
Diego santos <diegosan1955@hotmail.com>
2021-05-02 19:14:02 -0300

    Update title.js

--- scripts/title.js
+++ scripts/title.js
@@ -4,7 +4,7 @@ function typeWriter (elemento) {
     const textoArray = elemento.innerHTML.split('');
     elemento.innerHTML = '';
     textoArray.forEach((letra, i) => {
-        setTimeout(() => elemento.innerHTML += letra, 75 * i);       
+        setTimeout(() => elemento.innerHTML += letra, 70 * i);       
        });
       }
             
