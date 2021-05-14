82507b23c4dde462106cd3224ba490f472c4cd97
Diego Silva <41221742+odiegosilva1@users.noreply.github.com>
2021-05-14 22:36:36 +0100

    Update title.js

--- scripts/title.js
+++ scripts/title.js
@@ -1,11 +1,7 @@
 
 
 function typeWriter (elemento) {
-<<<<<<< HEAD
     const textoArray = elemento.innerHTML.split('_');
-=======
-     const textoArray = elemento.innerHTML.split('_');
->>>>>>> 34f2d2949b31329ce5e2595234d388a57dc4ac65
     elemento.innerHTML = '';
     textoArray.forEach((letra, i) => {
         setTimeout(() => elemento.innerHTML += letra, 70 * i);       
