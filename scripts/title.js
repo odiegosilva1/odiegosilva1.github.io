1d1fbcc038240ac97f3153c834733bd2607bd36c
Diego Silva <41221742+odiegosilva1@users.noreply.github.com>
2021-05-16 16:50:45 +0100

    Update title.js

--- scripts/title.js
+++ scripts/title.js
@@ -1,15 +1,12 @@
 
 
 function typeWriter (elemento) {
-    const textoArray = elemento.innerHTML.split('_');
+    const textoArray = elemento.innerHTML.split('');
     elemento.innerHTML = '';
     textoArray.forEach((letra, i) => {
         setTimeout(() => elemento.innerHTML += letra, 70 * i);       
        });
       }
             
-
-
-
 const título = document.querySelector('h1');
 typeWriter(título);
